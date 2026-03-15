8a58292c4838c96931d6864addc7ed00c3075539
Vivekananda <vivekanandadash245@gmail.com>
2026-03-16 00:31:23 +0530

    first commit

---
+++ src/main/java/com/example/java_docker_app/JavaDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_docker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDockerAppApplication.class, args);
+	}
+
+}
